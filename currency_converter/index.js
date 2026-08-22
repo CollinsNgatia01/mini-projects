4fde6b45c3e4b9a9eaec94f50f29c83ec1d6959c
Collins Ngatia <collinsng7@gmail.com>
2026-08-22 20:42:40 +0300

    currency_converter

---
+++ currency_converter/index.js
@@ -0,0 +1,28 @@
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate()
+
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/7b68dd11d703d16b4e84ae4e/latest/${currencyFirstEl.value}`).then((res)=>res.json()).then((data)=>{
+        const rate = data.conversion_rates [currencySecondEl.value]
+        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
+    });
+}
+
+currencyFirstEl.addEventListener("change", updateRate)
+
+currencySecondEl.addEventListener("change", updateRate)
+
+worthFirstEl.addEventListener("input", updateRate)
+
+

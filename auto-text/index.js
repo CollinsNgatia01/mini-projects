6ba28c5575fcd708fce38ba680aeb4d23db24744
Collins Ngatia <collinsng7@gmail.com>
2026-08-26 00:28:18 +0300

    auto-text

--- auto-text/index.js
+++ auto-text/index.js
@@ -1,14 +1,26 @@
-const containerEl = document.querySelector(".container")
+const containerEl = document.querySelector(".container");
 
-const careers = ["Yotuber", "Web developer", "Freelancer","Instructor"];
+const careers = ["Youtuber", "Web developer", "Freelancer", "Instructor"];
 
-let careerIndex = 0
+let careerIndex = 0;
 
 let characterIndex = 0;
 
 updateText();
 
-function updateText(){
-    containerEl.innerHTML = `<h1>I am a ${careers[careerIndex]}</h1>`;
-}
+function updateText() {
+    characterIndex++;
 
+    containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+
+    if (characterIndex === careers[careerIndex].length) {
+        careerIndex++;
+        characterIndex = 0;
+    }
+
+    if (careerIndex === careers.length) {
+        careerIndex = 0;
+    }
+
+    setTimeout(updateText, 400);
+}
